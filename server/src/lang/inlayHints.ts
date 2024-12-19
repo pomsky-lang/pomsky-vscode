ff9a2879169c072ac391fc614592b5e160e76ea1
Ludwig Stecher <ludwig.stecher@gmx.de>
2024-12-19 20:49:09 +0100

    fix inlay hints to ignore content in brackets/braces, and hide inlay hints if there are named capturing groups

--- server/src/lang/inlayHints.ts
+++ server/src/lang/inlayHints.ts
@@ -14,15 +14,29 @@ export function initInlayHints(documents: TextDocuments<TextDocument>) {
     const { tokens } = getInfo(model.uri, text, 'tokens')
 
     const hints: InlayHint[] = []
+    let nesting = 0
+    let index = 1
     for (let i = 0; i < tokens.length; i++) {
       const token = tokens[i]
-      if (token[0] === Token.Colon && tokens[i + 1]?.[0] === Token.OpenParen) {
-        const number = hints.length + 1
-        hints.push({
-          label: String(number),
-          position: model.positionAt(token[2]),
-          tooltip: `Capturing group #${number}`,
-        })
+      // don't consider colons in character sets and in tests
+      if (token[0] === Token.OpenBracket || token[0] === Token.OpenBrace) {
+        nesting++
+      } else if (token[0] === Token.CloseBracket || token[0] === Token.CloseBrace) {
+        nesting--
+      } else if (token[0] === Token.Colon && nesting === 0) {
+        if (tokens[i + 1]?.[0] === Token.OpenParen) {
+          hints.push({
+            label: String(index),
+            position: model.positionAt(token[2]),
+            tooltip: `Capturing group #${index}`,
+          })
+        } else {
+          // don't show number inlay hints if there are both named and unnamed capturing groups
+          // because Regex engines count them differently:
+          // https://www.regular-expressions.info/named.html#number
+          return []
+        }
+        index++
       }
     }
 
